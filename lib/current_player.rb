5e25c9ab923d3bffea3de248a7d97681467b99f7
Nahomie ND <nducasse3@gmail.com>
2021-01-27 04:56:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,11 @@
- board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  counter = 0
+ counter = 0
   def turn_count(board)
 
   board.each do |turn|
     
-  if turn == "x" || turn == "o"
-    counter +=1
-end
-end
+    if turn == "X" || turn == "O"
+      counter +=1
+    end
+  end
     puts "#{board}"
 end
-  turn_count(board)
